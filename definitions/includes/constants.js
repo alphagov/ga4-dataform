d15abf6814d2854d2670fbb5504a5d59e70b5f59
Ian Ansell <ian.ansell@digital.cabinet-office.gov.uk>
2026-01-02 14:40:58 +0000

    add placeholder constants for project and datasets

---
+++ definitions/includes/constants.js
@@ -0,0 +1,8 @@
+const processing_project_id = 'xxx';
+const output_project_id = 'xxx';
+const dataset = 'xxx';
+
+module.exports = { 
+    processing_project_id,
+    output_project_id,
+    dataset };
